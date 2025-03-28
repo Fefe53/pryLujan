aa480c6f42a5749df007be6e33aef1032be73c5c
Lujan Federico Yamil <fedelujan2016@gmail.com>
2025-03-28 13:58:27 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryLujan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Lindo viernes para estar con Visual Studio 2022!");
+
+        }
+
+    }
+}
